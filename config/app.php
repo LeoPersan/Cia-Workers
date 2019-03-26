3950e6467ddab09e852d106a16b745038dbea9cf
LeoPersan <leops1990@gmail.com>
2019-03-26 20:48:51 -0300

    Laravel Make Auth

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Cia&Workers'),
+    'name' => env('APP_NAME', 'Cias&Workers'),
 
     /*
     |--------------------------------------------------------------------------
